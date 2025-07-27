946e98c98467245d4ee0f2e68ebd1a77b2a18f2b
AbdulRafey476 <abdul.rafey476@hotmail.com>
2025-07-27 16:33:03 +0500

    add and configure dotenv for secure credentials

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+require('dotenv').config();
 
 const app = express();
 
 // DB Connection Using Mongoose
-mongoose.connect('mongodb+srv://AbdulRafey:NzL3cmFozf1CoTHx@cluster0.tnjqsig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=> console.log("Database Connected"));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Database Connected"));
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method')); // For Enabling Put and Del Request On Frontend
-
 
 // Routes
 app.get('/', (req, res) => {
