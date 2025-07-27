9481e3e92a1ed985dc137fa6e0c0ffc954438e41
AbdulRafey476 <abdul.rafey476@hotmail.com>
2025-07-27 16:21:11 +0500

    connected to mongodb atlas using mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 const app = express();
 
+// DB Connection Using Mongoose
+mongoose.connect('mongodb+srv://AbdulRafey:NzL3cmFozf1CoTHx@cluster0.tnjqsig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=> console.log("Database Connected"));
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
